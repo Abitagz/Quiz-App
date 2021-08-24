128f5e62a4c7c6fce55c9c81ea6f5c8a3d80a6ff
Abitagz <helloabita2005@yahoo.com>
2021-08-24 19:23:43 +0000

    Add heading, input and label to html, and  Qeustions to JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+const quizData = [
+    {
+      question: "When was William Shakespeare born?",
+      a: "25 September 1580",
+      b: "23 April 1564",
+      c: "12 June 1600",
+      d: "30 October",
+      correct: "b"
+    },{
+      question: "When was euro introduced as legal currency on the world market?",
+      a: "1 January 1999",
+      b: "20 January 2000",
+      c: "12 June 1998",
+      d: "1 February 1999",
+      correct: "a"
+    },{
+      question: "What is the largest freshwater lake in the world?",
+      a: "lake Superior",
+      b: "Nile",
+      c: "Lake Chad",
+      d: "Lake Balaton",
+      correct: "a"
+    },{
+      question: "What is the seventh planet from the sun?",
+      a: "Earth",
+      b: "Mars",
+      c: "Saturn",
+      d: "Uranus",
+      correct: "d"
+    },{
+      question: "What is the world's longest river?",
+      a: "Pacific",
+      b: "Amazon",
+      c: "Columbia",
+      d: "Nile",
+      correct: "d"
+    },{
+      question: "What is the diameter of Earth?",
+      a: "10,000miles",
+      b: "8,000miles",
+      c: "2000km",
+      d: "None of the above",
+      correct: "b"
+    },{
+      question: "Who played Neo in The Matrix?",
+      a: "Peter Jackson",
+      b: "David Ray",
+      c: "Keanu Reeves",
+      b: "Denzel Washington",
+      correct: "c"
+    },{
+      question: "Who directed the Lord of the Ring trilogy?",
+      a: "Peter Jackson",
+      b: "Andrew Peter",
+      c: "Reo Galvin",
+      d: "Bob Andrew",
+      correct: "a"
+    },{
+      question: "What is John Leach famous for making?",
+      a: "Music",
+      b: "Art",
+      c: "Dance",
+      d: "Pottery",
+      correct: "d"
+    },{
+      question: "What dose the musical term 'piano' mean?",
+      a: "To dance",
+      b: "Sing",
+      c: "To be played softly",
+      d: "Perform",
+      correct: "c"
+    }
+    
+  ];
